af3cb8f7e8e3e60b57798efc74337949dcf36ab8
ArtemShi <huebud@gmail.com>
2022-06-13 14:16:24 +0300

    create func to search information in work.ua

--- pars.py
+++ pars.py
@@ -1,11 +1,45 @@
 import requests
 import codecs
+from bs4 import BeautifulSoup
 
 headers = {}
+domain = 'https://www.work.ua'
 url = 'https://www.work.ua/ru/jobs-kyiv-python/'
 
 response = requests.get(url)
+jobs = []
+errors = []
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+    main_div = soup.find('div', {'id': 'pjax-job-list'})
+    if main_div:
+        div_lst = soup.find_all('div', attrs={'class': 'job-link'})
+        for div in div_lst:
+            title = div.find('h2')
+            href = title.a['href']
+            content = div.p.text
+            company = 'No name'
+            logo = div.find('img')
+            if logo:
+                company = logo['alt']
 
-handler = codecs.open('work_ua.html', 'w', 'utf-8')
-handler.write(str(response.text))
-handler.close()
+            jobs.append(
+                {
+                    'title': title.text,
+                    'url': domain + href,
+                    'description': content,
+                    'company': company
+                }
+            )
+    else:
+        errors.append({'url': url, 'title': "Div does not exists"})
+
+else:
+    errors.append({'url': url, 'title': "Page do not response"})
+# handler = codecs.open('work_ua.html', 'w', 'utf-8')
+# handler.write(str(response.text))
+# handler.close()
+
+# handler = codecs.open('work_ua.csv', 'w', 'utf-8')
+# handler.write(str(jobs))
+# handler.close()
