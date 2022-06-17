332a64c5823e08499d3162ade953e7d94102fd4e
ArtemShi <huebud@gmail.com>
2022-06-17 18:07:59 +0300

    add error model and update func

--- run_scraping.py
+++ run_scraping.py
@@ -2,28 +2,43 @@ import codecs
 import os
 import sys
 
+
+
 proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append('path_to+my_project/')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ScrapWork.settings'
 
 import django
 django.setup()
+from django.db import DatabaseError
 
 from scraping.pars import *
-from scraping.models import Vacancy, City, Language
+from scraping.models import Vacancy, City, Language, Errors
 
 parsers = (
     (work_ua, 'https://www.work.ua/ru/jobs-kyiv-python/'),
     (jobitt_scrap, 'https://jobitt.com/ru/job-openings?gclid=Cj0KCQjwwJuVBhCAARIsAOPwGAT3T3ntveS3nFWR8sM6k_3PSlhLiE_xazlzWcMsqzzRz92CEVTPtRUaAnROEALw_wcB&search=Python&page=1&city=265')
            )
 
-city = City.objects.filter(slug='kiev')
+city = City.objects.filter(slug='kiev').first()
+language = Language.objects.filter(slug='python').first()
+
 jobs, errors = [], []
 for func, url in parsers:
     j, e = func(url)
     jobs += j
     errors += e
 
-handler = codecs.open('work.txt', 'w', 'utf-8')
-handler.write(str(jobs))
-handler.close()
+for job in jobs:
+    vacancy = Vacancy(**job, city=city, language=language)
+    try:
+        vacancy.save()
+    except DatabaseError:
+        pass
+
+if errors:
+    er = Errors(data=errors).save()
+
+# handler = codecs.open('work.txt', 'w', 'utf-8')
+# handler.write(str(jobs))
+# handler.close()
--- scraping/models.py
+++ scraping/models.py
@@ -53,3 +53,16 @@ class Vacancy(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Errors(models.Model):
+
+    timestamp = models.DateField(auto_now_add=True)
+    data = models.JSONField()
+
+    class Meta:
+        verbose_name = 'Ошибка'
+        verbose_name_plural = 'Ошибки'
+
+    def __str__(self):
+        return self.timestamp
