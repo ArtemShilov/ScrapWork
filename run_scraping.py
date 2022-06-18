71a37c52a6b29d1251a62e2483c5c0aa49e22afd
ArtemShi <huebud@gmail.com>
2022-06-18 14:12:44 +0300

    add func get_setting

--- run_scraping.py
+++ run_scraping.py
@@ -2,6 +2,7 @@ import codecs
 import os
 import sys
 
+from django.contrib.auth import get_user_model
 
 proj = os.path.dirname(os.path.abspath('manage.py'))
 sys.path.append(proj)
@@ -14,11 +15,20 @@ from django.db import DatabaseError
 from scraping.pars import *
 from scraping.models import Vacancy, City, Language, Errors
 
+User = get_user_model()
+
 parsers = (
     (work_ua, 'https://www.work.ua/ru/jobs-kyiv-python/'),
     (jobitt_scrap, 'https://jobitt.com/ru/job-openings?gclid=Cj0KCQjwwJuVBhCAARIsAOPwGAT3T3ntveS3nFWR8sM6k_3PSlhLiE_xazlzWcMsqzzRz92CEVTPtRUaAnROEALw_wcB&search=Python&page=1&city=265'),
     (jobs_ua, 'https://jobs.ua/vacancy/kiev/rabota-python'),
            )
+
+
+def get_settings():
+    qs = User.objects.filter(send_email=True).values()
+    settings_list = set((q['city_id'], q['language_id']) for q in qs)
+    return settings_list
+
 
 city = City.objects.filter(slug='kiev').first()
 language = Language.objects.filter(slug='python').first()
