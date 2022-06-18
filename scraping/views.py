3c56378a88f1f324d96fea0ed263799a2fe48512
ArtemShi <huebud@gmail.com>
2022-06-18 18:45:49 +0300

    create pagination

--- scraping/views.py
+++ scraping/views.py
@@ -13,7 +13,7 @@ def list_view(request):
     form = FindForm()
     city = request.GET.get('city')
     language = request.GET.get('language')
-    page_obj = []
+    context = {'city': city, 'language': language, 'form': form}
     if city or language:
         _filter = {}
         if city:
@@ -26,5 +26,6 @@ def list_view(request):
 
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
+        context['object_list'] = page_obj
 
-    return render(request, 'list.html', {'object_list': page_obj, 'form': form})
+    return render(request, 'list.html', context)
